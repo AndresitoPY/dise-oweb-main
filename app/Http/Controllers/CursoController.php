f61a444b4f3bf3217d65b0594bb90476a1f09f70
JuanKier <juanandreskl1999@gmail.com>
2022-10-14 16:44:12 -0300

    proyecto

---
+++ app/Http/Controllers/CursoController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Curso;
+use Illuminate\Http\Request;
+use Flash;
+
+class CursoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $cursos = Curso::all();
+         return view('cursos.index',compact('cursos'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('cursos.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $cursos= request()->except('_token');
+        Curso::insert($cursos);
+        Flash::success('Creado correctamente');
+        return redirect (route('cursos.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\cr  $cr
+     * @return \Illuminate\Http\Response
+     */
+    public function show(cr $cr)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\cr  $cr
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $cursos=Curso::findorFail($id);
+        return view ('cursos.edit', compact('cursos'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\cr  $cr
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request,  $id)
+    {
+      $cursos=request()->except(['_token','_method']);
+      Curso::where('id','=',$id)->update($cursos);
+       Flash::success('Actualizado correctamente');
+        return redirect ('cursos');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\cr  $cr
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy( $id)
+    {
+        Curso::destroy($id);
+        Flash::error('Eliminado correctamente');
+        return redirect('cursos');
+    }
+}
